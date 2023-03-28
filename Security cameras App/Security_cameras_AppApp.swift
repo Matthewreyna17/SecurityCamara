b649af5f5a02556ca205d3f4cd1b4908e0790f19
Matthew Reyna <matthewreyna17@gmail.com>
2023-03-28 05:44:19 -0500

    Initial Commit

---
+++ Security cameras App/Security_cameras_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Security_cameras_AppApp.swift
+//  Security cameras App
+//
+//  Created by Matthew Reyna on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Security_cameras_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
